b6d17612f1d063bbfc597a1d422116d486c2cbd3
Mehdi EA <mehdi.el-aamrani.1@ens.etsmtl.ca>
2021-12-20 20:16:47 -0500

    modification mineur de test

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div class='mt-20'>
       <Logo style='width:120px;margin:auto' />
       <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
+        Premier pas dans le game
       </h1>
       <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
+        C'est un truc de malade frerou
       </h2>
       
     </div>
